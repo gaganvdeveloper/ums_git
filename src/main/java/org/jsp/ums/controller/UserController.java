379c2f0f48760e84903eb7ffe00bb487bbef1941
gaganvdeveloper <gaganvdeveloper@gmail.com>
2024-08-25 13:09:48 +0530

    .

--- src/main/java/org/jsp/ums/controller/UserController.java
+++ src/main/java/org/jsp/ums/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,7 +29,7 @@ public class UserController {
 //	@PutMapping        													// Vikas and Group
 	
 	
-//	@GetMapping(value =  "/{id}")   												// Bharath  & vamshi & Venkatesh
+//	@GetMapping(value =  "/{id}")   									// Bharath  & vamshi & Venkatesh
 	
 	
 //	@GetMapping     													// Misba and Group
