2e46b6e59dab88abadc9671e196ee82019d0931a
Venkatesh-akula <venkateshakula159@gmail.com>
2024-08-26 13:05:28 +0530

    putmapping venkatesh

--- src/main/java/org/jsp/ums/controller/EducationController.java
+++ src/main/java/org/jsp/ums/controller/EducationController.java
@@ -1,9 +1,14 @@
 package org.jsp.ums.controller;
 
+import org.jsp.ums.entity.Education;
+import org.jsp.ums.service.EducationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/educations")
 public class EducationController {
 	
+	@Autowired
+	private EducationService educationservice;
 	
 	
 	
@@ -18,7 +25,10 @@ public class EducationController {
 	
 	
 	
-//	@PutMapping  					Venkatesh
+	@PutMapping
+	public ResponseEntity<?> updateEducationDetails(@RequestBody Education education){
+		return educationservice.serviceUpdateEducation(education);
+	}
 	
 	
 //	@GetMapping("/{id}") 			Ranjitha
